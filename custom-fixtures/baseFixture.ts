6662f95b0a61ab5d46d634b31e4b8b82a68cc076
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-08-02 15:30:39 +0530

    Added a new test case

--- custom-fixtures/baseFixture.ts
+++ custom-fixtures/baseFixture.ts
@@ -1,9 +1,18 @@
 import { test as base } from "@playwright/test";
 import { UserCreds } from "../modal-objects/UserCreds";
-import * as userdata from "../data/user-creds.json";
+import * as userdata from "../data/user/user-creds.json";
 
 export const test = base.extend<{ userCreds: UserCreds }>({
   userCreds: new UserCreds(userdata),
 });
+
+export const addInfo = (testDetails: object) => {
+  for (const testInfo in testDetails) {
+    base.info().annotations.push({
+      type: testInfo,
+      description: testDetails[testInfo],
+    });
+  }
+};
 
 export { expect } from "@playwright/test";
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,32 @@
 import { App } from "../page-objects/App";
-import { test, expect } from "../custom-fixtures/baseFixture";
-
-test.beforeEach(async ({ page }) => {
-  await page.goto("/login");
-});
+import { test, expect, addInfo } from "../custom-fixtures/baseFixture";
 
 test.describe("Login - e2e test", () => {
-  test("Should be able to login", async ({ page, userCreds }) => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/login");
+  });
+  test("Should be able to login with valid user credentials", async ({
+    page,
+    userCreds,
+  }) => {
+    addInfo({
+      Owner: "Sanjeev Kumar",
+      UserStory: "US-1",
+    });
+    const app = new App(page);
+    await app.LoginPage.login(userCreds.username, userCreds.password);
+    await expect(page).toHaveTitle("The Internet");
+    await app.LoginPage.isUserlogged();
+  });
+
+  test("Should not be able to login with invalid user credentials", async ({
+    page,
+    userCreds,
+  }) => {
+    addInfo({
+      Owner: "Sanjeev Kumar",
+      UserStory: "US-2",
+    });
     const app = new App(page);
     await app.LoginPage.login(userCreds.username, userCreds.password);
     await expect(page).toHaveTitle("The Internet");
