9a4f5e99b09858fc4a50196bda84e9653fcb7804
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-08-02 14:30:35 +0530

    renamed the variable in fixture file

--- tests/fixtures/baseFixture.ts
+++ tests/fixtures/baseFixture.ts
@@ -2,8 +2,8 @@ import { test as base } from "@playwright/test";
 import { UserCreds } from "../../modal-objects/UserCreds";
 import * as userdata from "../../data/user-creds.json";
 
-const test = base.extend<{ UserCreds: UserCreds }>({
-  UserCreds: new UserCreds(userdata),
+const test = base.extend<{ userCreds: UserCreds }>({
+  userCreds: new UserCreds(userdata),
 });
 
 export default test;
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,9 +7,9 @@ test.beforeEach(async ({ page }) => {
 });
 
 test.describe("Login - e2e test", () => {
-  test("Should be able to login", async ({ page, UserCreds }) => {
+  test("Should be able to login", async ({ page, userCreds }) => {
     const app = new App(page);
-    await app.LoginPage.login(UserCreds.username, UserCreds.password);
+    await app.LoginPage.login(userCreds.username, userCreds.password);
     await expect(page).toHaveTitle("The Internet");
     await app.LoginPage.isUserlogged();
   });
