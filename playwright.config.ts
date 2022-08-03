74cc15906a02452f061df84987b98fee51508dc4
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-08-03 17:33:36 +0530

    Updated script

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,12 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   testDir: "./tests",
-  timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
+  timeout: 60000,
   reporter: [["html", { outputFolder: "test-report" }]],
   use: {
     viewport: { width: 1280, height: 720 },
     baseURL: "https://the-internet.herokuapp.com",
-    headless: true,
+    headless: false,
     screenshot: "only-on-failure",
   },
 };
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,21 +1,11 @@
 import { App } from "../page-objects/App";
-import { test, expect, addInfo } from "../custom-fixtures/baseFixture";
+import { test, expect } from "../custom-fixtures/baseFixture";
 
 test.describe("Login - e2e test", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/login");
   });
   test("Should be able to login with valid user credentials", async ({
-    page,
-    userCreds,
-  }) => {
-    const app = new App(page);
-    await app.LoginPage.login(userCreds.username, userCreds.password);
-    await expect(page).toHaveTitle("The Internet");
-    await app.LoginPage.isUserlogged();
-  });
-
-  test("Should not be able to login with invalid user credentials", async ({
     page,
     userCreds,
   }) => {
