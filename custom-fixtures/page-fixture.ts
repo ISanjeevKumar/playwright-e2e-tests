bfbce351ab75185dda200848acc9070ecea586d6
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-12-06 14:54:42 +0530

    [Sanjeev] updated custom fixture

--- custom-fixtures/page-fixture.ts
+++ custom-fixtures/page-fixture.ts
@@ -1,11 +1,19 @@
 import { test as base } from "@playwright/test";
 import { RegisterPage } from "../page-objects/RegisterPage";
+import * as userdata from "../data/user/register-data.json";
+import { RegisterModal } from "../modal-objects/RegisterModal";
 
 type Pages = {
   RegisterPage: RegisterPage;
 };
 
-export const test = base.extend<Pages>({
+type Testdata = {
+  registerdata: RegisterModal;
+};
+export const test = base.extend<Pages & Testdata>({
+  registerdata: async ({}, use) => {
+    await use(new RegisterModal(userdata));
+  },
   RegisterPage: async ({ page }, use) => {
     await use(new RegisterPage(page));
   },
--- tests/fixture-based-pattern/register.spec.ts
+++ tests/fixture-based-pattern/register.spec.ts
@@ -1,17 +1,10 @@
 import { expect, test } from "../../custom-fixtures/page-fixture";
-import * as userdata from "../../data/user/register-data.json";
-import { RegisterModal } from "../../modal-objects/RegisterModal";
 
 test.describe("Register new user", function () {
-  let registerdata: RegisterModal;
-
-  test.beforeEach(async () => {
-    registerdata = new RegisterModal(userdata);
-  });
-
   test("Should be able to register new user", async ({
     page,
     RegisterPage,
+    registerdata,
   }) => {
     await RegisterPage.navigateToRegister();
     await expect(page).toHaveTitle("Register Account");
