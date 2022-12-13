f54fa2060189d4efc9d716751abbe1620602733f
sanjeev.kumar1 <sanjeev.kumar1@thoughtworks.com>
2022-12-13 16:27:12 +0530

    [Sanjeev] Added missing dependencies

--- config/global-config.ts
+++ config/global-config.ts
@@ -2,7 +2,7 @@ import { FullConfig } from "@playwright/test";
 const fse = require("fs-extra");
 
 async function globalSetup(config: FullConfig) {
-  await fse.remove("/test-report");
+  await fse.remove("/test-results");
   console.log("Playwright config:", config);
 }
 
